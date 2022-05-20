acaf4a7c7a3df53bfc04e5a523c4bb2bde30e94f
PhongTQ <phongtq2@smartosc.com>
2022-05-20 17:17:01 +0700

    mange cubit flow route

--- lib/main.dart
+++ lib/main.dart
@@ -4,47 +4,23 @@ import 'package:flutter_app/persentation/screens/second_screen.dart';
 import 'package:flutter_app/persentation/screens/third_screen.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
+import 'persentation/router/app_router.dart';
 import 'persentation/screens/home_screen.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
-class MyApp extends StatefulWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  // This widget is the root of your application.
-  final CounterCubit _counterCubit = CounterCubit();
+class MyApp extends StatelessWidget {
+  final AppRouter _appRouter = AppRouter();
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      routes: {
-        '/': (context) => BlocProvider.value(
-              value: _counterCubit,
-              child: const HomeScreen(
-                  title: 'HomeScreen', color: Colors.redAccent),
-            ),
-        '/second': (context) => BlocProvider.value(
-              value: _counterCubit,
-              child: const SecondScreen(title: 'second', color: Colors.blue),
-            ),
-        '/third': (context) => BlocProvider.value(
-              value: _counterCubit,
-              child: const ThirdScreen(title: 'third', color: Colors.orange),
-            ),
-      },
+    return BlocProvider(
+      create: (context) => CounterCubit(),
+      child: MaterialApp(
+        onGenerateRoute: _appRouter.onGenerateRoute,
+      ),
     );
-  }
-
-  @override
-  void dispose() {
-    _counterCubit.close();
-    super.dispose();
   }
 }
